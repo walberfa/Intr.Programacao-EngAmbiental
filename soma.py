ca6c42cbfc743b2fb2e0dc8be8fcc0329d03cb4f
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2022-10-17 23:18:55 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,6 @@
+# Código simples de uma soma entre dois números inteiros inseridos pelo usuário
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print('O resultado da soma é: ', soma)
