4d3e3a4e60ccedacc3e5ebd0dd64f0b5bafb31e5
walberfa <walberfa@gmail.com>
2024-05-15 22:03:26 -0300

    [Refact] Add opcoes no menu.

--- src/triangulos.py
+++ src/triangulos.py
@@ -1,20 +1,24 @@
 """Código usado para classificar um triangulo de acordo com os angulos inseridos pelo usuário."""
 
-angulo_a = float(input("Digite o valor do angulo A: "))
-angulo_b = float(input("Digite o valor do angulo B: "))
-angulo_c = float(input("Digite o valor do angulo C: "))
 
-soma = angulo_a + angulo_b + angulo_c
-print("Soma dos angulos internos é igual a", soma)
-
-if soma != 180:
-    print("Os valores são inválidos para um triangulo")
-elif angulo_a == 0 or angulo_b == 0 or angulo_c == 0:
-    print("Um dos valores é igual a zero, logo não é um triangulo")
-else:
-    if angulo_a == 90 or angulo_b == 90 or angulo_c == 90:
-        print("É um triangulo retangulo")
-    elif angulo_a > 90 or angulo_b > 90 or angulo_c > 90:
-        print("É um triangulo obtusangulo")
+def verifica_se_valido(a, b, c):
+    soma = a + b + c
+    if soma != 180:
+        print("Os valores são inválidos para um triangulo")
+        return False
+    elif a == 0 or b == 0 or c == 0:
+        print("Um dos valores é igual a zero, logo não é um triangulo")
+        return False
     else:
-        print("É um triangulo acutangulo")
+        return True
+
+
+def define_tipos(angulo_a, angulo_b, angulo_c):
+    if angulo_a == 90 or angulo_b == 90 or angulo_c == 90:
+        resultado = "retangulo"
+    elif angulo_a > 90 or angulo_b > 90 or angulo_c > 90:
+        resultado = "obtusangulo"
+    else:
+        resultado = "acutangulo"
+    print(f'É um triangulo {resultado}.')
+    return resultado
--- src/verificacao_idades.py
+++ src/verificacao_idades.py
@@ -1,16 +1,18 @@
 """O código a seguir usa operadores lógicos para verificar se as idades inseridas pelo usuário
 são de pessoas maiores de 18 anos."""
 
-idade1 = int(input("Digite a primeira idade: "))
-idade2 = int(input("Digite a segunda idade: "))
 
-# Lógica com o operador 'and'
-if idade1 >= 18 and idade2 >= 18:
-    print("Os dois são maiores de idade.")
-else:
-    print("Pelo menos um dos dois não é maior de idade.")
-# Lógica com o operador 'or'
-if idade1 >= 18 or idade2 >= 18:
-    print("Pelo menos um dos dois é maior de idade.")
-else:
-    print("Os dois são menores de idade")
+def verificacao_idade(idade1, idade2):
+    # Lógica com o operador 'and'
+    if idade1 >= 18 and idade2 >= 18:
+        resposta = "Os dois são maiores de idade."
+    else:
+        resposta = "Pelo menos um dos dois não é maior de idade."
+    # Lógica com o operador 'or'
+    if idade1 >= 18 or idade2 >= 18:
+        resposta = "Pelo menos um dos dois é maior de idade."
+    else:
+        resposta = "Os dois são menores de idade."
+
+    print(resposta)
+    return resposta
